336688888b163774288919dec3469250e0b67a85
matridaniel052-glitch <matridaniel052@gmail.com>
2026-05-05 14:36:14 +0400

    Update config for SQLite deployment

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
+import os
+
 class Config:
-    SECRET_KEY = 'supermarket-secret-2024'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/supermarket_db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'supermarket-secret-2024'
+    
+    # Use SQLite for deployment, MySQL for local development
+    if os.environ.get('RAILWAY_ENVIRONMENT') or os.environ.get('RENDER'):
+        # Production environment (Railpack, Railway, Render, etc.)
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'supermarket.db')
+    else:
+        # Local development environment
+        SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/supermarket_db'
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
